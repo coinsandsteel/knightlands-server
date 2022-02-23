6809d91ec406169782774bd3402ef925f1dcf16f
tran.duc <tranduc2702@gmail.com>
2022-02-23 20:43:15 +0700

    [K1-350] fix: Modify probability for raid rewards

--- src/raids/raid.js
+++ src/raids/raid.js
@@ -565,14 +565,14 @@ class Raid extends EventEmitter {
                     rewards.items.push(...Game.lunarManager.getRaidReward());
                 }
                 if (Game.marchManager.eventIsInProgress()) {
-                    const isGetMarchReward = Random.range(0, 1) < 0.05; // 5% chance to get eventTicket
+                    const isGetMarchReward = Random.range(0, 1) < (0.05 * chosenLoot.damageThreshold / raidStage.maxSlots);
                     isGetMarchReward && rewards.items.push(Game.marchManager.getRaidReward());
                 }
             } else {
                 let winLoot = user.isFreeAccount ? baseLoot.winnerLootFree : baseLoot.winnerLootNormal;
                 rewards.items.push(...await Game.lootGenerator.getLootFromTable(winLoot));
                 if (Game.marchManager.eventIsInProgress()) {
-                    const isGetMarchReward = Random.range(0, 1) < 0.30; // 30% chance to get eventTicket
+                    const isGetMarchReward = Random.range(0, 1) < (0.30 * chosenLoot.damageThreshold / raidStage.maxSlots);
                     isGetMarchReward && rewards.items.push(Game.marchManager.getRaidReward());
                 }
             }
