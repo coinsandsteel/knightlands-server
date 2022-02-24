f54a6c741de0959d0dca4bb1f753cf828a749fbe
tran.duc <tranduc2702@gmail.com>
2022-02-24 10:38:04 +0700

    fix: fix probability of event reward for solo raid

--- src/raids/raid.js
+++ src/raids/raid.js
@@ -565,7 +565,7 @@ class Raid extends EventEmitter {
                     rewards.items.push(...Game.lunarManager.getRaidReward());
                 }
                 if (Game.marchManager.eventIsInProgress()) {
-                    const isGetMarchReward = Random.range(0, 1) < (0.05 * chosenLoot.damageThreshold / raidStage.maxSlots);
+                    const isGetMarchReward = Random.range(0, 1) < 0.05;
                     isGetMarchReward && rewards.items.push(Game.marchManager.getRaidReward());
                 }
             } else {
