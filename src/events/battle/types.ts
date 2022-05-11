bf83414d450a8b16700ca484794c54e12e9c669a
uniwertz <uniwertz@gmail.com>
2022-05-11 21:43:19 +0400

    chg: types.ts

--- src/events/battle/types.ts
+++ src/events/battle/types.ts
@@ -1,5 +1,3 @@
-import { NumberLiteralType } from "typescript";
-
 export interface BattleSaveData {
   user: BattleUserState;
   game: BattlGameState;
@@ -8,9 +6,12 @@ export interface BattleSaveData {
 export interface BattleUserState {
   balance: {
     energy: number;
-    gold: number;
-    coins: number;
-    crystals: number;
+    coins: number; // PvE, upgrade unit level
+    crystals: number; // PvP, upgrade ability level
+  };
+  timers: {
+    // TODO research timers
+    energy: number;
   };
   rewards: {
     dailyRewards: BattleRewardDayData[];
@@ -29,14 +30,15 @@ export interface BattleRewardRankingData {
 }
 
 export interface BattlGameState {
-  room: number;
-  level: number;
+  room: number; // 8
+  difficulty: number; // 0, 1
+  level: number; // 5 + 1
   turn: boolean;
   squads: {
     user: BattleSquad;
     enemy: BattleSquad;
   };
-  map: BattleMapCell[];
+  terrain: BattleTerrainCell[];
 }
 
 export interface BattleSquad {
@@ -46,47 +48,63 @@ export interface BattleSquad {
 }
 
 export interface BattleUnit {
-  unitClass: string;
-  index: number;
+  id?: string;
+  unitClass: string; // 5
+  // exp > max limit > pay coins > lvl up > attributes auto-upgrade
+  level: number; // 15
+  tier: number; // 3, modify via merger (3 => 1)
   power: number;
-  tier: number;
+  index: number; // 0-34
   exp: number;
-  level: number;
   abilities: BattleUnitAbility[];
   attributes: BattleUnitAttribute[];
-  coverageArea: number[];
-  // hp:
-  // damage:
-  // defence:
-  // speed: 
-  // initiative:
+  moveCells: number[]; // Choosed "move" action
+  attackCells: number[]; // Choosed ability
 }
 
-export interface BattleMapCell {
-  cellClass: string;
+export interface BattleTerrainCell {
+  terrainClass: string; // >= 5
+  index: number;
 }
 
 export interface BattleUnitAbility {
-  alias: string;
-  level: number;
-  initial: number;
-  current: number;
-  max: number;
-  cooldown: number; // turns
+  abilityClass: string; // 
+  // unit lvl opens ability lvl > pay crystal > lvl up
+  level: number; // 3-5
+  value: {
+    initial: number;
+    current: number;
+  };
+  cooldown: {
+    initial: number;
+    current: number;
+  };
+  buffs: BattleBuff[];
 }
 
 export interface BattleUnitAttribute {
-  alias: string;
+  attributeClass: string;
   level: number;
   initial: number;
   current: number;
-  max: number;
+}
+
+export interface BattleBuff {
+  buffClass: string;
+  delta: number;
+  modifier: number; // % > ceil()
+  cooldown: number;
 }
 
 export interface BattleSquadBonus {
+  // magic attack
+  // phys attack
+  // magic defence
+  // phys defence
+  // terrain
+  // speed
   alias: string;
-  level: number;
-  initial: number;
-  current: number;
-  max: number;
+  // TODO details
+  value: number; // % or delta
+  modifier: number;
 }
