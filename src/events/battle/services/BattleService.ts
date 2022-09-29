60beaece5216b5a817386e7e590d49aa18091cbf
uniwertz <uniwertz@gmail.com>
2022-09-29 14:18:31 +0400

    fix: Remove logs

--- src/events/battle/services/BattleService.ts
+++ src/events/battle/services/BattleService.ts
@@ -1,8 +1,8 @@
 
 export class BattleService {
   public log(message: string, payload?: any) {
-    //if (process.env.ENV === "development" || process.env.ENV === "dev") {
+    if (process.env.ENV === "development" || process.env.ENV === "dev") {
       console.log(`[${this.constructor.name}] ${message}`, payload);
-    //}
+    }
   }
 }
