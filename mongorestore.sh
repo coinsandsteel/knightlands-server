2cca1af7b89ca9e0c18db8d6b52f0ab1adb0a335
uniwertz <uniwertz@gmail.com>
2022-10-02 20:55:23 +0400

    chg: Ugrade mongorestore script

--- mongorestore.sh
+++ mongorestore.sh
@@ -1,2 +1,4 @@
 rm -rf /srv/dump
+tar -xvf /srv/dump.tar
 mongorestore --host localhost:27017 --db=knightlands --username=test --authenticationDatabase=admin --drop /srv/dump/knightlands
+cd /srv/knightlands-server && pm2 stop kl && pm2 start ecosystem.config.js
