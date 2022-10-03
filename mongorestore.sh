14e915fa85c34207da688118b890bd84a0075e6d
uniwertz <uniwertz@gmail.com>
2022-10-03 21:04:06 +0400

    chg: Restore script ready

--- mongorestore.sh
+++ mongorestore.sh
@@ -1,4 +1,4 @@
 rm -rf /srv/dump
 cd /srv && tar -xvf dump.tar
-mongorestore --host localhost:27017 --username=test --authenticationDatabase=admin --drop /srv/dump/knightlands
+mongorestore --host localhost:27017 --db=knightlands --username=test --authenticationDatabase=admin --drop /srv/dump/knightlands
 cd /srv/knightlands-server && pm2 stop kl && pm2 start ecosystem.config.js
