940949fd675a909031b2ebf856d61fd122e0693b
uniwertz <uniwertz@gmail.com>
2022-10-06 17:36:33 +0400

    chg: Remove logs

--- src/events/battle/services/BattleInventory.ts
+++ src/events/battle/services/BattleInventory.ts
@@ -62,14 +62,14 @@ export class BattleInventory extends BattleService {
 
     // Add new unit
     const newUnitEntry = Unit.createUnit(newUnitMeta, this._core.events);
-    console.log('Merge abilities', {
+    /*console.log('Merge abilities', {
       source: unit.abilities.abilities,
       target: newUnitEntry.abilities.abilities
-    });
+    });*/
 
     // Set the same level
     newUnitEntry.setLevel(unit.levelInt, true);
-    console.log('Merge set level', unit.levelInt);
+    //console.log('Merge set level', unit.levelInt);
 
     // Unlock abilities
     newUnitEntry.addExpirience(0);
@@ -78,7 +78,7 @@ export class BattleInventory extends BattleService {
     newUnitEntry.abilities.abilities.forEach(ability => {
       const sourceUnitAbilityData = unit.abilities.getAbilityByClass(ability.abilityClass);
       if (sourceUnitAbilityData.enabled) {
-        console.log('Merge ability', { abilityClass: ability.abilityClass, levelInt: sourceUnitAbilityData.levelInt });
+        //console.log('Merge ability', { abilityClass: ability.abilityClass, levelInt: sourceUnitAbilityData.levelInt });
         newUnitEntry.abilities.setAbilityLevel(
           ability.abilityClass,
           sourceUnitAbilityData.levelInt
